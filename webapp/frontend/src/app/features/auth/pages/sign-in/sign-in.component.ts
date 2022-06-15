c3a5190bf8455771a640b60edec26352c06a183f
kuzkovandrey <kuzkovandrey@gmail.com>
2022-06-15 22:14:28 +0300

    Created base sign up page

--- webapp/frontend/src/app/features/auth/pages/sign-in/sign-in.component.ts
+++ webapp/frontend/src/app/features/auth/pages/sign-in/sign-in.component.ts
@@ -1,8 +1,16 @@
+import { AppRoutes } from '@core/values/app-routes.enum';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
 })
-export class SignInComponent {}
+export class SignInComponent {
+  constructor(private readonly router: Router) {}
+
+  navigateToSignUp() {
+    this.router.navigate([AppRoutes.AUTH, AppRoutes.SIGN_UP], { replaceUrl: true });
+  }
+}
--- webapp/frontend/src/app/features/auth/pages/sign-up/sign-up.component.ts
+++ webapp/frontend/src/app/features/auth/pages/sign-up/sign-up.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppRoutes } from '@core/values/app-routes.enum';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+
+enum SignUpStates {
+  EMAIL = 'email',
+  PASSWORD = 'password',
+}
+
+enum Subtitles {
+  EMAIL = 'Введите свой email',
+  PASSWORD = 'Придумайте пароль',
+}
 
 @Component({
   selector: 'sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
 })
-export class SignUpComponent {}
+export class SignUpComponent {
+  readonly signUpStates = SignUpStates;
+
+  readonly currentSignUpState$ = new BehaviorSubject<SignUpStates>(SignUpStates.EMAIL);
+
+  readonly subtitle$: Observable<string> = this.currentSignUpState$.pipe(
+    map((state) => (state === SignUpStates.EMAIL ? Subtitles.EMAIL : Subtitles.PASSWORD)),
+  );
+
+  constructor(private readonly router: Router) {}
+
+  navigateToSignIn() {
+    this.router.navigate([AppRoutes.AUTH, AppRoutes.SIGN_IN], { replaceUrl: true });
+  }
+}
